63b5193de47757b00b613487ae123a595b549418
Veneta <venkireva80@gmail.com>
2022-05-09 11:15:58 +0300

    Container tests

--- test.cpp
+++ test.cpp
@@ -1,7 +1,34 @@
 #define CATCH_CONFIG_MAIN
 #include "catch.hpp"
 
-TEST_CASE ("Test")
+#include "warehouse.hpp"
+
+TEST_CASE ("Container tests")
 {
-    REQUIRE(true);
+    SECTION ("Default constructor")
+    {
+        Container c1;
+        REQUIRE(c1.getCapacity() == 0);
+    }
+
+    SECTION ("Constructor with parameters")
+    {
+        Container c1(5);
+        REQUIRE(c1.getCapacity() == 5);
+    }
+
+    SECTION ("Copy constructor")
+    {
+        Container c1(5);
+        Container c2(c1);
+        REQUIRE(c1.getCapacity() == 5);
+        REQUIRE(c2.getCapacity() == 5);
+    }
+
+    SECTION ("Operator =") {
+        Container c1(10);
+        Container c2 = c1;
+        REQUIRE(c1.getCapacity() == 10);
+        REQUIRE(c2.getCapacity() == 10);
+    }
 }
