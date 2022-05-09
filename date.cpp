11cfa30a289afb802a0f75bf50a8a6af7a878a5f
Veneta <venkireva80@gmail.com>
2022-05-09 11:40:33 +0300

    Make 0 invalid month and day for Date

--- date.cpp
+++ date.cpp
@@ -41,7 +41,7 @@ Date::~Date() {
 
 
 bool Date::validate() {
-    if (day < 0 || month < 0 || day > 31 || month > 12 || year < 0) {
+    if (day < 1 || month < 1 || day > 31 || month > 12 || year < 0) {
         return false;
     }
 
