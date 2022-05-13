61641d3adee072a93bbd9239fa800d5aeb314d78
Veneta <venkireva80@gmail.com>
2022-05-13 09:17:56 +0300

    Add documentation for placement

--- placement.hpp
+++ placement.hpp
@@ -1,9 +1,15 @@
 #ifndef __PLACEMENT
 #define __PLACEMENT
 
+///\brief Representation of a product's placement in warehouse
 struct Placement {
+    /// The section the product is in
     int section = -1;
+
+    /// The shelf the product is on
     int shelf = -1;
+
+    /// The index the product is at
     int index = -1;
 };
 
